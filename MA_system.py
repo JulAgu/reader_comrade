296835d416e5b37aad6eb8bc93b2b976eb6769df
JulAgu <jsagudeloac@unal.edu.co>
2025-02-17 19:00:55 +0100

    Add an architecture idea

--- MA_system.py
+++ MA_system.py
@@ -1,19 +1,30 @@
 from typing import Literal
-from pydantic import BaseModel, Field
 from langchain_mistralai import ChatMistralAI
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.pydantic_v1 import BaseModel, Field, validator, Literal
+import yaml
+with open("system_prompts.yaml", "r") as file:
+    prompts = yaml.safe_load(file)
 
 mistral_model = "mistral-large-2411"
 llm = ChatMistralAI(model=mistral_model, temperature=0)
 
 # Definig DataClasses for deterministic outputs
 class AcceptPrompt(BaseModel):
-    """ Route a user query to the most relevant datasource. """
-
-    datasource: Literal["answer", "refuse"] = Field(
+    """
+    Accept or reject a question.
+    """
+    answ_decision: Literal["answer", "refuse"] = Field(
         ...,
         description="Given a user question choose to answer or refuse it.",
     )
+    @validator("answ_decision")
+    def validate_datasource(cls, v):
+        if v == "answer" or v == "refuse":
+            return v
+        else:
+            raise ValueError("datasource must be either 'answer' or 'refuse'")
+
 
 class RouteQuery(BaseModel):
     """ Route a user query to the most relevant datasource. """
@@ -22,6 +33,13 @@ class RouteQuery(BaseModel):
         ...,
         description="Given a user question choose to route it to web search or a vectorstore.",
     )
+    @validator("datasource")
+    def validate_datasource(cls, v):
+        if v == "vectorstore" or v == "websearch":
+            return v
+        else:
+            raise ValueError("datasource must be either 'vectorstore' or 'websearch'")
+
 def make_acceptRouter():
     structured_llm_router = llm.with_structured_output(AcceptPrompt)
     # TODO: Verify how to add memory to this
